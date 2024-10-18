71e5fa4a071902f40bdbbd0950040ecc72bf9ee2
Logan <126ldevane@frhsd.com>
2024-10-18 13:58:09 -0400

    done

---
+++ js/sandbox.js
@@ -0,0 +1,17 @@
+
+const age = 30;
+
+if(true) {
+    const age= 40;
+ const name = 'shaun';
+ console.log('inside 2nd code block: ', age);
+
+
+
+if(true) {
+    const age= 50;
+ console.log('inside 2nd code block: ', age);
+
+}
+}
+ console.log ('outside code block: ' , age, name, test);
